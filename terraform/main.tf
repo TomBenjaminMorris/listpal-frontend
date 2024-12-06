343274f2ecb496ee4eaa9cb557a3607e826a313d
Tom Morris <thomasmorris823@gmail.com>
2024-12-06 11:38:04 +0000

    bump tf module version

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,21 +20,23 @@ provider "aws" {
 }
 
 locals {
-  project_name      = "listpal-dev"
-  domain_name       = "listpal.dev.vinsp.in"
-  root_domain_name  = "vinsp.in"
-  allowed_locations = ["GB"]
+  project_name         = "listpal-dev"
+  domain_name          = "listpal.dev.vinsp.in"
+  root_domain_name     = "vinsp.in"
+  allowed_locations    = ["GB"]
+  forward_query_string = true
   common_tags = {
     Project = "listpal.dev.vinsp.in"
   }
 }
 
 module "static-website" {
-  source = "git@github.com:TomBenjaminMorris/tf-modules.git//static-website?ref=static-website-v0.0.14"
+  source = "git@github.com:TomBenjaminMorris/tf-modules.git//static-website?ref=static-website-v0.0.15"
 
-  project_name      = local.project_name
-  domain_name       = local.domain_name
-  root_domain_zone  = local.root_domain_name
-  allowed_locations = local.allowed_locations
-  common_tags       = local.common_tags
+  project_name         = local.project_name
+  domain_name          = local.domain_name
+  root_domain_zone     = local.root_domain_name
+  allowed_locations    = local.allowed_locations
+  common_tags          = local.common_tags
+  forward_query_string = local.forward_query_string
 }
